fc105606d466809b9f589ebaeff7fb0478b0cc1b
rishab <rishab.singh@userfacet.com>
2022-01-06 18:26:25 +0530

    Forst

--- pr1/pols/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Student(models.Model):
-                 s_id=models.AutoField
-                 s_name=models.CharField(max_length=50)
-                 course=models.CharField(max_length=50)
-                 adm_date=models.DateField()
--- pr1/pr1/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def home_view(request):
-	return HttpResponse("home PR1")
-
-
-	
